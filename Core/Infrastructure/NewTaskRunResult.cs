1da204fbc10d4b547d2eff1ba7cd4170db04457c
Chris Rogers <crogers@exemail.com.au>
2016-12-22 12:34:27 +1100

    Added AdditionalSettings to the task run parameters

--- Core/Infrastructure/NewTaskRunResult.cs
+++ Core/Infrastructure/NewTaskRunResult.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace DataRetention.Core.Infrastructure
 {
@@ -11,5 +12,49 @@ namespace DataRetention.Core.Infrastructure
         public DateTime DateFrom { get; set; }
 
         public DateTime DateTo { get; set; }
+
+        // note that other custom settings may be provided here which some robots will need.
+        // WARNING: this is not yet fleshed out - but an example of what it may look like it below....
+        private AdditionalSettings _additionalSettings = new AdditionalSettings();
+        public AdditionalSettings AdditionalSettings
+        {
+            get { return _additionalSettings; }
+            set { _additionalSettings = value; }
+        }
+    }
+
+    public class AdditionalSettings
+    {
+        private Dictionary<string, dynamic> _settings = new Dictionary<string, dynamic>();
+        public Dictionary<string, dynamic> Settings
+        {
+            get { return _settings; }
+            set { _settings = value; }
+        }
+
+        public T GetSettingAs<T>(string key)
+        {
+            return _settings.ContainsKey(key) && _settings[key] is T ? _settings[key] : null;
+        }
+
+        public string GetSettingAsString(string key)
+        {
+            return GetSettingAs<string>(key);
+        }
+
+        public int? GetSettingAsInt(string key)
+        {
+            return GetSettingAs<int>(key);
+        }
+
+        public bool? GetSettingAsBool(string key)
+        {
+            return GetSettingAs<bool>(key);
+        }
+
+        public DateTime? GetSettingAsDateTime(string key)
+        {
+            return GetSettingAs<DateTime>(key);
+        }
     }
 }
--- DataRetention.Robot.Test1/DummyTaskServer.cs
+++ DataRetention.Robot.Test1/DummyTaskServer.cs
@@ -24,11 +24,15 @@ namespace DataRetention.Robot.Test1
 
         public ActionResponse<NewTaskRunResult> RequestNewTaskRun()
         {
-            return new ActionResponse<NewTaskRunResult>
+            var newTaskRunResult = new NewTaskRunResult
                 {
-                    Success = true,
-                    Response = new NewTaskRunResult { RunRequired = true, SessionId = "unique-session-id-123", DateFrom = new DateTime(2016, 12, 1), DateTo = new DateTime(2016, 12, 2) }
+                    RunRequired = true,
+                    //SessionId = "unique-session-id-123",
+                    SessionId = Guid.NewGuid().ToString(),
+                    DateFrom = new DateTime(2016, 12, 1),
+                    DateTo = new DateTime(2016, 12, 2)
                 };
+            return new ActionResponse<NewTaskRunResult> { Success = true, Response = newTaskRunResult };
         }
 
         public ActionResponse Starting(string sessionId)
